3d6a0081a54a3f7823303ac098be2166d0dd363d
noSpaghettiCode <btzemil@gmail.com>
2021-08-29 00:00:54 +0300

    added adjustements

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import {
   StyleSheet,
   Text,
   View,
   FlatList,
   TouchableOpacity,
+  Animated,
 } from "react-native";
 
 import { StatusBar } from "expo-status-bar";
@@ -23,26 +24,42 @@ import slides from "../utils/slides";
 import OnboardingItem from "../components/OnboardingItem";
 
 const OnboardingScreen = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const scrollX = useRef(new Animated.Value(0)).current;
+  const slidesRef = useRef(null);
+  const viewableItemsChanged = useRef(({ viewableItems }) => {
+    setCurrentIndex(viewableItems[0].index);
+  }).current;
+
+  const viewConfig = useRef({viewAreaCoveragePercentThreshold: 50}).current;
 
   const Skip = () => {
-      return (
-        <TouchableOpacity>
-            <Text>Skip</Text>
-        </TouchableOpacity>
-      )
+    return (
+      <TouchableOpacity>
+        <Text>Skip</Text>
+      </TouchableOpacity>
+    );
   };
 
   return (
     <View style={styles.container}>
-      <FlatList
+     <View style={{flex: 3}}>
+     <FlatList
         data={slides}
         renderItem={({ item }) => <OnboardingItem item={item} />}
         horizontal
         showsHorizontalScrollIndicator={false}
         pagingEnabled
         bounces={false}
+        keyExtractor={(item) => item.id}
+        scrollEventThrottle={32}
+        onScroll={Animated.event([{ nativeEvent: { contentOffset: { x: scrollX } } }], {
+            useNativeDriver: false,
+        })}
+        onViewableItemsChanged={viewableItemsChanged}
+        ref={slidesRef}
       />
-      
+     </View>
     </View>
   );
 };
